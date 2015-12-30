b32ebfab7661649c10ffd6dc75f8aa9021b58c47
Michael Gattozzi <mgattozzi@gmail.com>
2015-12-29 20:13:41 -0500

    Made so script fails at problem

--- install.sh
+++ install.sh
@@ -28,39 +28,39 @@ then
 fi
 
 #Update Submodules
-git submodule init
-git submodule update
+git submodule init &&
+git submodule update &&
 
 # Install the programs
-cd xdo && make && sudo make install && cd ..
-cd bar && make && sudo make install && cd ..
-cd sxhkd && make && sudo make install && cd ..
-cd xtitle && make && sudo make install && cd ..
-cd sutils && make && sudo make install && cd ..
-cd bspwm && make && sudo make install && cd ..
+cd xdo && make && sudo make install && cd .. &&
+cd bar && make && sudo make install && cd .. &&
+cd sxhkd && make && sudo make install && cd .. &&
+cd xtitle && make && sudo make install && cd .. &&
+cd sutils && make && sudo make install && cd .. &&
+cd bspwm && make && sudo make install && cd .. &&
 
 #Create needed directories
-mkdir -p $BSPWM
-mkdir -p $SXHKD
+mkdir -p $BSPWM &&
+mkdir -p $SXHKD &&
 
 #Create symlinks for bspwm config files/directories
-cd $BSPWM
-ln -s $CONFIG/bspwmrc .
-ln -s $CONFIG/panel .
-ln -s $CONFIG/panel_bar .
-ln -s $CONFIG/panel_colors .
+cd $BSPWM &&
+ln -s $CONFIG/bspwmrc . &&
+ln -s $CONFIG/panel . &&
+ln -s $CONFIG/panel_bar . &&
+ln -s $CONFIG/panel_colors . &&
 
 #Create symlinks for sxhkd config files/directories
-cd $SXHKD
-ln -s $CONFIG/sxhkdrc .
+cd $SXHKD &&
+ln -s $CONFIG/sxhkdrc . &&
 
 #Symlink wallpaper to Home Directory
-cd $HOME
-ln -s $CONFIG/.wallpaper.jpg .
-cd $CONFIG
+cd $HOME &&
+ln -s $CONFIG/.wallpaper.jpg . &&
+cd $CONFIG &&
 
 #Setup necessary variables
-sudo su -c 'echo PANEL_FIFO="/tmp/panel-fifo" >> /etc/profile'
+sudo su -c 'echo PANEL_FIFO="/tmp/panel-fifo" >> /etc/profile' &&
 sudo su -c 'echo PATH=$PATH:$HOME/.config/bspwm >> /etc/profile'
 
 #Allow logging in via bspwm
