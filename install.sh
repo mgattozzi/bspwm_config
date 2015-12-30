e6833bd09eab16b5dca6c3e14d928fe810989854
Michael Gattozzi <mgattozzi@gmail.com>
2015-12-29 21:26:43 -0500

    Finished install Script for Ubuntu

--- install.sh
+++ install.sh
@@ -21,10 +21,14 @@ if [[ $OS == "Ubuntu" ]]
 then
   sudo apt-get update
   sudo apt-get install build-essential git bash xcb libxcb-util0-dev libxcb-ewmh-dev libxcb-randr0-dev libxcb-icccm4-dev libxcb-keysyms1-dev libxcb-xinerama0-dev libasound2-dev libx11-xcb-dev libxcb-xtest0-dev
+  #Programs used in scripts
+  sudo apt-get install terminator dmenu feh
 elif [[ $OS == "Arch" ]]
 then
   sudo pacman -Syy
   sudo pacman -S git bash libxcb xcb-util xcb-util-wm xcb-util-keysyms alsa-libbase-devel xorg-xinit xorg-server xorg-server-common xorg-server-devel xorg-server-utils xorg-xinput
+  #Programs used in scripts
+  sudo pacman -S terminator dmenu feh
 fi
 
 #Update Submodules
@@ -60,8 +64,8 @@ ln -s $CONFIG/.wallpaper.jpg . &&
 cd $CONFIG &&
 
 #Setup necessary variables
-sudo su -c 'echo export PANEL_FIFO="/tmp/panel-fifo" >> /etc/profile' &&
-sudo su -c 'echo export PATH=$PATH:$HOME/.config/bspwm >> /etc/profile'
+sudo echo export PANEL_FIFO="/tmp/panel-fifo" >> /etc/profile &&
+sudo echo export PATH=$PATH:$HOME/.config/bspwm >> /etc/profile
 
 #Allow logging in via bspwm
 if [[ $OS == "Ubuntu" ]]
@@ -70,7 +74,6 @@ then
 elif [[ $OS == "Arch" ]]
 then
   touch $HOME/.xinitrc
-  echo 'sxhkd &' >> $HOME/.xinitrc
   echo 'exec bspwm' >> $HOME/.xinitrc
   echo "Check your .xinitrc to make sure it's setup properly"
 fi
